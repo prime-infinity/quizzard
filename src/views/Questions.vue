75f9ff1cc5d50a41911b78a1c01f3e786f8f7663
prime-infinity <soe.a.prime@gmail.com>
2021-12-22 01:49:06 +0100

    i am master of code

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -19,7 +19,7 @@
                 <input type="radio" :id="`three${question.id}`" value="C" v-model="objAnswers.optionChosen[question.id]">
                 <label :for="`three${question.id}`">C</label>{{ question.optionC }}<br>
 
-                <!--<span v-if="revealAnswers">ANSWER: {{ question.answer }}</span>  <br>
+                <span v-if="revealAnswers">ANSWER: {{ question.answer }}</span>  <br>
 
                 <div v-if="revealAnswers">
                     <div class="failedQuestio" v-if="question.answer != questionStatus(question.id)">
@@ -28,7 +28,7 @@
                     <div class="gottenQuestio" v-if="question.answer == questionStatus(question.id)">
                         correct
                     </div>
-                </div>-->
+                </div>
 
             </div>
 
@@ -78,6 +78,24 @@ export default {
         },
         submitAnswers:function(){
             this.revealAnswers = true
+            //console.log(this.questions);
+            /*console.log(this.questions.map(({ answer }) => answer));
+            console.log(Object.values(this.objAnswers.optionChosen));*/
+
+            let realAns = this.questions.map(({ answer }) => answer)
+            let givenAns = Object.values(this.objAnswers.optionChosen)
+            
+            let count = 0
+
+            for (let i = 0; i < realAns.length; i++) {
+                const ans = realAns[i];
+              
+                if(ans == givenAns[i]){
+                    count ++;
+                }
+
+            }
+            console.log(count);
 
         }
     },
